eea02b51ea846cf63a299975050567fda3ace6a8
Jose Mendes <josemendesper24@gmail.com>
2025-02-05 16:09:24 +0100

    Ajout de la fonction FizzBuzz

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 
-def main():
-    print('Hello, World!')
-    return 0
 
-if __name__ == '__main__':
-    main()
-
+def fizzbuzz(n):
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+fizzbuzz(20)
